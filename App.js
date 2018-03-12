87b610579d6680f3f7238b988ada2c9f8d34f0d6
sarath <sarath.sct@gmail.com>
2018-03-12 16:06:48 +0530

    initial

---
+++ App.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/navbar';
+
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route
+                        exact
+                        path="/"
+                        component={Home}
+                    />
+                </div>
+            </BrowserRouter>
+
+        );
+    }
+}
+
---
+++ components/navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+
+export default class extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand href="/">fundraiser</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">SignUp</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Login</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
